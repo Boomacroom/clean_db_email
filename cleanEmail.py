5a1e8cc6a7c8f465cd567d2750600b2624fce978
Christopher Irick <mymagitech@gmail.com>
2021-02-15 09:26:18 -0500

    Function and for loop created with all delimiters

--- cleanEmail.py
+++ cleanEmail.py
@@ -1,46 +1,60 @@
 '''
 A client had an email field and didn't do much in the way of sanitizing data in the past
 They also didn't do any data validation on the field.
-Returning a DB query for COMPANY and with EMAIL, variations of the following
-would be returned.
+Returning a DB query for COMPANY and with EMAIL, the following would return
 
 COMPANY         EMAIL
-Test Company    1@abc.com; 2@abc.com; 3@abc.com
-Test Company 2  1@cba.com/2@cba.com/3@cba.com
-Test Company 3  1@bcd.com//2@bcd.com//3@bcd.com
+Test Company    1@abc.com; 2@abc.com;
+Test Company 2  2@abc.com/3@abc.com/person@company.com
 
-
-Employees in the past used whatever delimiter they liked sometimes a / sometimes //
+Employees in the past used whatever delimeter they liked sometimes a / sometimes //
 and sometimes just a space. I wrote this to help separate out the emails while retaining
 the COMPANY name for each email.
+
+Adapted from an answer on Stack Overflow that I can't seem to locate anymore.
 '''
+
 import pandas as pd
+import os, glob
+import numpy as np
 
-# creating a data frame from CSV where the CSV
-# is a result of a db query returning COMPANY and EMAIL
-df = pd.read_csv("themall.csv") 
+def extractEmails(df, lst_cols, fill_value='', preserve_index=False):
+    # make sure `lst_cols` is list-alike
+    if (lst_cols is not None
+        and len(lst_cols) > 0
+        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
+    # preserve original index values
+    idx = np.repeat(df.index.values, lens)
+    # create "exploded" DF
+    res = (pd.DataFrame({
+                col:np.repeat(df[col].values, lens)
+                for col in idx_cols},
+                index=idx)
+             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
+                            for col in lst_cols}))
+    # append those rows that have empty lists
+    if (lens == 0).any():
+        # at least one list in cells is empty
+        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
+                  .fillna(fill_value))
+    # revert the original index order
+    res = res.sort_index()
+    # reset index if requested
+    if not preserve_index:        
+        res = res.reset_index(drop=True)
+    return res
 
-# Split all by ;
-df2 = pd.concat([pd.Series(row['COMPANY'], row['EMAIL'].split(';'))
-    for _, row in df.iterrows()]).reset_index()
+# read in csv
+df = pd.read_csv("themall.csv")
+# Set all possible delimiters - Each one of these was found to have
+# been found used in the actual database.
+delimiters = ['; ', ';', ':', '/////', '////', '///', '//', '/', ' ']
 
-# Split by /// We start with the largest amount of /// and work our way down
-# to prevent it splitting where it shouldn't turning // into / for example
-
-df2 = pd.concat([pd.Series(row['COMPANY'], row['EMAIL'].split('///'))
-    for _, row in df.iterrows()]).reset_index()
-
-# Split all by //
-df2 = pd.concat([pd.Series(row['COMPANY'], row['EMAIL'].split('//'))
-    for _, row in df.iterrows()]).reset_index()
-
-# Split all by '/'
-df2 = pd.concat([pd.Series(row['COMPANY'], row['EMAIL'].split('/'))
-    for _, row in df.iterrows()]).reset_index()
-
-# Split all by SPACE
-df2 = pd.concat([pd.Series(row['COMPANY'], row['EMAIL'].split(' '))
-    for _, row in df.iterrows()]).reset_index()
-    
-#Write everything back to a CSV as export.
-df2.to_csv('themallall.csv', index=False)
+for delimiter in delimiters:
+    df = extractEmails(df.assign(EMAIL=df.EMAIL.str.split(delimiter)), 'EMAIL')
+df.to_csv('themall.csv', index=False)
